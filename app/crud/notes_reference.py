e4b4d1042beb0d3b5d6712129dcd6c2970b270ea
Junior Léo Bernard <jleobernard@gmail.com>
2022-10-17 15:47:11 +0200

    WIP - better retry policy

--- app/crud/notes_reference.py
+++ app/crud/notes_reference.py
@@ -1,9 +1,9 @@
 import datetime
 import logging
 import os
-from threading import Lock
+from threading import Lock, RLock
 from typing import Optional, List
-
+import time
 import aiohttp
 from pydantic import parse_obj_as
 
@@ -15,49 +15,71 @@ logger = logging.getLogger(__name__)
 
 class NotesReferenceDAO(metaclass=Singleton):
     note_backend_url: str
-    jwt_token: str
-    expires: int
-    pwd: str
-    login: str
-    _lock: Lock
+    max_retries: int
+    sleep_timeout: int
+    _jwt_token: str
+    _expires: int
+    _pwd: str
+    _login: str
+    _lock: RLock
+    _nb_retries: int
 
-    def __init__(self):
-        self.expires = -1
-        self.jwt_token = ""
+    def __init__(self, max_retries: int = 5, sleep_timeout: int = 1):
+        self._expires = -1
+        self.max_retries = max_retries
+        self.sleep_timeout = sleep_timeout
+        self._nb_retries = 0
+        self._jwt_token = ""
         self.note_backend_url = os.getenv('NOTES_BE_URL')
-        self.pwd = os.getenv('NOTES_BE_PASSSWORD')
-        self.login = os.getenv('NOTES_BE_LOGIN')
+        self._pwd = os.getenv('NOTES_BE_PASSSWORD')
+        self._login = os.getenv('NOTES_BE_LOGIN')
         for key in ['NOTES_BE_URL', 'NOTES_BE_LOGIN', 'NOTES_BE_PASSSWORD']:
             if not os.getenv(key):
                 raise RuntimeError(f"Set {key} environment variable")
-        self._lock = Lock()
+        self._lock = RLock()
 
     async def authenticate(self, force: bool = False):
         with self._lock:
-            if force or datetime.datetime.now().timestamp() >= self.expires:
+            if force or datetime.datetime.now().timestamp() >= self._expires:
                 async with aiohttp.ClientSession() as session:
-                    payload = {"username": self.login, "password": self.pwd}
+                    payload = {"username": self._login, "password": self._pwd}
                     async with session.post(f'{self.note_backend_url}/api/login', json=payload) as response:
                         if response.status == 200:
                             answer: dict = await response.json()
-                            self.jwt_token = answer["accessToken"]
+                            self._jwt_token = answer["accessToken"]
+                        elif response.status == 401:
+                            self._jwt_token = ''
+                            logger.error(f"Supplied credentials cannot be used to authenticate user")
+                            raise ConnectionError(
+                                f"Error while trying to connect as {self._login} to {self.note_backend_url}")
                         else:
-                            logger.error(f"Error {response.status} while trying to connect as {self.login} to"
+                            logger.error(f"Error {response.status} while trying to connect as {self._login} to"
                                          f" {self.note_backend_url}")
                             body = await response.read()
                             logger.error(f"Response body is {body}")
-                            raise ConnectionError(
-                                f"Error while trying to connect as {self.login} to {self.note_backend_url}")
+                            if self._nb_retries <= self.max_retries:
+                                time.sleep(self.sleep_timeout)
+                                self._nb_retries += 1
+                                await self.authenticate(force=force)
+                            else:
+                                raise ConnectionError(
+                                    f"Error while trying to connect as {self._login} to {self.note_backend_url}")
 
     async def fetch_note_by_uri(self, note_uri: str) -> Optional[NoteReferential]:
         await self.authenticate()
         async with aiohttp.ClientSession() as session:
             async with session.get(f'{self.note_backend_url}/api/notes/{note_uri}',
                                    headers={
-                                       'authorization': f"Bearer {self.jwt_token}"
+                                       'authorization': f"Bearer {self._jwt_token}"
                                    }) as response:
-                answer: dict = await response.json()
-                return NoteReferential(**answer)
+                if response.status == 401 and self._nb_retries <= self.max_retries:
+                    self._nb_retries += 1
+                    return await self.fetch_note_by_uri(note_uri=note_uri)
+                elif response.status == 200:
+                    answer: dict = await response.json()
+                    return NoteReferential(**answer)
+                else:
+                    raise ConnectionError(f"An error occurred while searching note {note_uri} ")
 
     async def fetch_notes(self, count: int, offset: int) -> List[NoteReferential]:
         await self.authenticate()
@@ -65,7 +87,12 @@ class NotesReferenceDAO(metaclass=Singleton):
         async with aiohttp.ClientSession() as session:
             async with session.get(f'{self.note_backend_url}/api/notes',
                                    headers={
-                                       'authorization': f"Bearer {self.jwt_token}"
+                                       'authorization': f"Bearer {self._jwt_token}"
                                    }, params=params) as response:
-                answer: dict = await response.json()
-                return parse_obj_as(List[NoteReferential], answer)
+                if response.status == 401 and self._nb_retries <= self.max_retries:
+                    self._nb_retries += 1
+                elif response.status == 200:
+                    answer: dict = await response.json()
+                    return parse_obj_as(List[NoteReferential], answer)
+                else:
+                    raise ConnectionError(f"An error occurred while searching notes with count = {count} and offset = {offset} ")
